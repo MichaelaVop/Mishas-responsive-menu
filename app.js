c6cbc6b19cb8b08ed18e5ab59cee5f4051d1f5a7
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-02-07 22:47:13 +0100

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-
-
 function myFunction() {
     const x = document.getElementById("myMenu");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
+    if (x.className === "myLinks") {
+        x.className += " responsive";
     } else {
-        x.style.display = "flex";
+        x.className = "myLinks";
     }
-  }
+}
+
+// function myFunction() {
+//     const x = document.getElementById("myMenu");
+//     if (x.style.display === "flex") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "flex";
+//     }
+//   }
+
+/*
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+*/
