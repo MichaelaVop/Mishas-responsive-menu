5deefedd8186034f37024b3cfcd754c0fb9de4f9
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-02-07 05:55:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function myFunction() {
+    const x = document.getElementById("myMenu");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+  }
