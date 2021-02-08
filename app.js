69ca6bd0e52a0cc7c314acfb77abf9c3dc7c1a6b
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-02-08 14:43:27 +0100

    third commit

--- app.js
+++ app.js
@@ -6,16 +6,3 @@ function myFunction() {
         x.className = "myLinks";
     }
 }
-
-// function myFunction() {
-//     const x = document.getElementById("myMenu");
-//     if (x.style.display === "flex") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "flex";
-//     }
-//   }
-
-/*
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-*/
